fdd5cb064a0c22b09264c7e0bb43119382c02851
Sjd-Bh <s.bahonar.bio@gmail.com>
2024-07-18 21:42:48 +0330

    fivth comit

--- SinglCellSim/AmpSim/MDAsubsetMulticore_python.py
+++ SinglCellSim/AmpSim/MDAsubsetMulticore_python.py
@@ -3,53 +3,50 @@ import multiprocessing
 import time
 import sys
 import os
-import logging
 import numpy as np
+#np.random.seed(42)  # Set a specific seed value, such as 42
+import random
+#random.seed(42)
+#sys.path.append('/home/bahonar/simulation/SingleCellSim')
+# Ensure the script is run from the project root directory
 current_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.abspath(os.path.join(current_dir, os.pardir, os.pardir))
 sys.path.insert(0, project_root)
 from SinglCellSim.AmpSim.MDAstate import MDASimulation, subsetAmpliconSaveToFASTA
 from SinglCellSim.configs.configFunctions import read_config, read_fasta
 
-logging.basicConfig(level=logging.DEBUG)  # Set DEBUG level to capture all messages
-
 def run_simulation(sim_index, args, config_params):
-    try:
-        logging.debug(f"Running simulation {sim_index} with PID {os.getpid()}")
-        logging.debug(f"Config params at start of simulation {sim_index}: {config_params}")
-        
-        np.random.seed((os.getpid() * int(time.time())) % 123456789)
-        output_folder = os.path.join(args.output_base, f"sim{sim_index}")
-        os.makedirs(output_folder, exist_ok=True)
-        
-        Theta = int(config_params['theta'])
-        Gamma = int(config_params['gamma'])
-        DNACoef = int(config_params['dnacoef'])
-        lMin = int(config_params['lmin'])
-        lMax = int(config_params['lmax'])
-        Lambda = float(config_params['lambda'])
-        delta_t = float(config_params['delta_t'])
-        beta = float(config_params['beta'])
-        exclude = config_params['exclude']
+    np.random.seed((os.getpid() * int(time.time())) % 123456789)
+    output_folder = os.path.join(args.output_base, f"sim{sim_index}")
+    os.makedirs(output_folder, exist_ok=True)
+    print(f"Config parameters: {config_params}")
+    
+    Theta = int(config_params['theta'])
+    Gamma = int(config_params['gamma'])
+    DNACoef = int(config_params['dnacoef'])
+    lMin = int(config_params['lmin'])
+    lMax = int(config_params['lmax'])
+    Lambda = float(config_params['lambda'])
+    delta_t = float(config_params['delta_t'])
+    beta = float(config_params['beta'])
+    exclude = config_params['exclude']
 
-        start_time = time.time()
+    start_time = time.time()
 
-        # Read patSeq and matSeq from FASTA files
-        patSeq_data = read_fasta(args.patSeq_file)
-        matSeq_data = read_fasta(args.matSeq_file)
-        template = args.tem
-        
-        amplicons = MDASimulation(patSeq_data, matSeq_data, Theta=Theta, Gamma=Gamma, DNACoef=DNACoef,
-                                  lMin=lMin, lMax=lMax, Lambda=Lambda, delta_t=delta_t, beta=beta, 
-                                  exclude=exclude, template=template, output_folder=output_folder)
-        
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        logging.debug(f"Elapsed time for simulation {sim_index}: {elapsed_time} seconds")
+    # Read patSeq and matSeq from FASTA files
+    patSeq_data = read_fasta(args.patSeq_file)
+    matSeq_data = read_fasta(args.matSeq_file)
+    template = args.tem
+    
+    amplicons = MDASimulation(patSeq_data, matSeq_data, Theta=Theta, Gamma=Gamma, DNACoef=DNACoef,
+                              lMin=lMin, lMax=lMax, Lambda=Lambda, delta_t=delta_t, beta=beta, 
+                              exclude=exclude, template = template,output_folder=output_folder)
+    
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Elapsed time for simulation {sim_index}: {elapsed_time} seconds")
 
-        subsetAmpliconSaveToFASTA(amplicons, patSeq_data, matSeq_data, output_folder=output_folder)
-    except Exception as e:
-        logging.error(f"Error in simulation {sim_index}: {e}")
+    subsetAmpliconSaveToFASTA(amplicons, patSeq_data, matSeq_data, output_folder=output_folder)
 
 def main():
     parser = argparse.ArgumentParser(description="Run MDA simulations and save subsets to FASTA")
@@ -68,20 +65,15 @@ def main():
     # Read parameters from the configuration file
     config_params = read_config(args.config_file, "Simulation")
 
-    logging.debug(f"Configuration parameters: {config_params}")
+    # Convert string values to appropriate types
+    #config_params = {key: int(value) if value.isdigit() else float(value) if '.' in value else value
+    #                 for key, value in config_params.items()}
+    print("Keys in config_params:", config_params.keys())
+    np.random.seed((os.getpid() * int(time.time())) % 123456789)
+    with multiprocessing.Pool(processes=args.num_cores) as pool:
+        pool.starmap(run_simulation, [(sim_index, args, config_params) for sim_index in range(1, args.num_simulations + 1)])
 
-    try:
-        # Temporarily run simulations sequentially for debugging
-        for sim_index in range(1, args.num_simulations + 1):
-            run_simulation(sim_index, args, config_params)
-
-        # Uncomment below for multiprocessing after debugging
-        # with multiprocessing.Pool(processes=args.num_cores) as pool:
-        #     pool.starmap(run_simulation, [(sim_index, args, config_params) for sim_index in range(1, args.num_simulations + 1)])
-    except Exception as e:
-        logging.error(f"Error in multiprocessing: {e}")
-
-    logging.debug("All simulations completed.")
+    print("All simulations completed.")
 
 if __name__ == "__main__":
     main()
