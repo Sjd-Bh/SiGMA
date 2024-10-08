454bc9c27e9e06da2eb8b3c0c3a2abb1fd5f491a
Sjd-Bh <s.bahonar.bio@gmail.com>
2024-10-08 21:25:19 +0330

    new changes

--- evaluation/comparePlots/csvFilesTOplot.py
+++ evaluation/comparePlots/csvFilesTOplot.py
@@ -11,27 +11,15 @@ sys.path.insert(0, project_root)
 from evaluation.configs.configFunctions import read_config
 from evaluation.comparePlots.simSCVtoPlot import MDA_PTA_VAF_DP_separation,calculate_ado,calculate_correlations,calculate_mvd, cor_plot,depth_plot,mvd_diff_mp,plot_ado  
 
-import glob
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='SNP calling on single cells')
-    parser.add_argument('--csv_files', nargs='+', type=str, help='List of paths to single-cell data files or directories containing them')
+    parser.add_argument('--csv_files', nargs='+', type=str, help='List of paths to single-cell data files')
     parser.add_argument('--output_folder', type=str, help='output file path and name')
     parser.add_argument('--key1', type=str, help='MDA or COCAllow')
     parser.add_argument('--key2', type=str, help='PTA or COCnotAllow')
     parser.add_argument("--config_file", type=str, default="mvdConfig.ini", help="Path to config.ini file")
     args = parser.parse_args()
-    
-    # Collect all CSV files from provided directories or file paths
-    csv_files = []
-    for path in args.csv_files:
-        if os.path.isdir(path):
-            # Recursively find all CSV files in the directory
-            csv_files.extend(glob.glob(os.path.join(path, "*.csv")))
-        else:
-            # Add file directly if it's a file
-            csv_files.append(path)
-
+    csvFiles = args.csv_files
     key1 = args.key1
     key2 = args.key2
     output_folder = args.output_folder
@@ -39,7 +27,7 @@ if __name__ == "__main__":
     min_values = config_params['min_values']
     max_values = config_params['max_values']
 
-    for csvFile in csv_files:
+    for csvFile in csvFiles:
         mvd_diff_mp(csvFile, key1, key2, output_folder)
         depth_plot(csvFile, key1, key2, output_folder)
         cor_plot(csvFile, min_values, max_values, key1, key2, output_folder)
