a9add6dc17b794cbafe6bb386419b4dd57963b02
Sjd-Bh <s.bahonar.bio@gmail.com>
2024-10-28 16:19:55 +0330

    new changes

--- evaluation/realDataAnalysis/realCsvToPlotFunctions.py
+++ evaluation/realDataAnalysis/realCsvToPlotFunctions.py
@@ -215,7 +215,7 @@ def depth_plot(csv_file, output_folder):
     plt.xlabel('Sample')
     plt.ylabel('Depth')
     plt.title('Box Plot of Depth for MDA and PTA Samples (Outliers Removed)')
-    plt.xticks(rotation=45)
+    plt.xticks(rotation=90)
     plt.tight_layout()
     plt.savefig(output_plot)
     plt.show()  # Optional: show the plot after saving
