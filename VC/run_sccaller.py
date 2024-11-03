b7104e72a4ed41937e95c514b55242eae44629b5
Sjd-Bh <s.bahonar.bio@gmail.com>
2024-11-03 22:38:56 +0330

    new changes

---
+++ VC/run_sccaller.py
@@ -0,0 +1,59 @@
+import argparse
+import glob
+import os
+import subprocess
+
+import sys
+
+# Ensure the script is run from the project root directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+bulkSim_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+sys.path.insert(0, bulkSim_dir)
+
+def run_sccaller(ref, sc_files, vcf_files, output_dir, bulk,core):
+    for sc_bam, sc_vcf in zip(sc_files, vcf_files):
+        sample_name = os.path.basename(os.path.dirname(sc_bam)) 
+        sample_output_dir = os.path.join(output_dir, sample_name)
+        
+        # Ensure output subdirectory for each sample exists
+        os.makedirs(sample_output_dir, exist_ok=True)
+
+        # Run sccaller using conda run in the SingleCellSim environment
+        output_vcf = os.path.join(sample_output_dir, f"sccaller_{sample_name}.vcf")
+        sccaller_command = [
+            "conda", "run", "-n", "Sccaller", "python2.7", "sccaller_v2.0.0.py", 
+            "-b", sc_bam,
+            "-f ", ref,
+            "-o",output_vcf,
+            "--bulk", bulk,
+            "-t", "hsnp",
+            "-s", sc_vcf,
+            "-n", core   
+        ]
+        print(f"Running sccaller for {sc_bam}")
+        subprocess.run(sccaller_command, check=True)
+        
+
+def main():
+    parser = argparse.ArgumentParser(description="Run ProSolo and convert VCF to BCF.")
+    parser.add_argument("--ref", required=True, help="Path to the reference FASTA file.")
+    parser.add_argument("--scFiles", required=True, help="Glob pattern for single-cell BAM files.")
+    parser.add_argument("--vcf", required=True, help="Glob pattern for VCF files.")
+    parser.add_argument("--bulk", required=True, help="Path to bulk BAM file.")
+    parser.add_argument("--out", required=True, help="Output directory for ProSolo and BCF files.")
+    parser.add_argument("--core", required=int(), help="number of threads")
+    args = parser.parse_args()
+
+    # Expand file paths using glob
+    sc_files = sorted(glob.glob(args.scFiles))
+    vcf_files = sorted(glob.glob(args.vcf))
+    
+    if len(sc_files) != len(vcf_files):
+        print("Error: Number of BAM files does not match the number of VCF files.")
+        return
+
+    os.makedirs(args.out, exist_ok=True)
+    run_sccaller(args.ref, sc_files, vcf_files, args.out, args.bulk,args.core)
+
+if __name__ == "__main__":
+    main()
