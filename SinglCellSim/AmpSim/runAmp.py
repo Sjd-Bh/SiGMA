558ce4d272624775ee90dbe4877b2c4368764c84
Sjd-Bh <s.bahonar.bio@gmail.com>
2026-08-25 14:37:36 +0330

    new changes

--- SinglCellSim/AmpSim/runAmp.py
+++ SinglCellSim/AmpSim/runAmp.py
@@ -13,7 +13,7 @@ import numpy as np
 # --------------------------------------------------
 current_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.abspath(
-    os.path.join(current_dir, os.pardir, os.pardir, os.pardir)
+    os.path.join(current_dir, os.pardir, os.pardir)
 )
 sys.path.insert(0, project_root)
 
